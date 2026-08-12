da25735d1340ccc9e34b5205a2d62e767ac034e5
Mike Quisido <mike082112@gmail.com>
2026-08-12 14:13:38 +0800

    Add KMCQ URL Checkpoint text below shield image

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <Image
         src="/images/orange-shield.png"
         alt="Orange shield"
         width={256}
         height={256}
       />
+      <p className="text-sm text-gray-500">KMCQ URL Checkpoint</p>
     </div>
   );
 }
